64f16c7e823979e805dc3037ed85f308d98c4c8e
meenakshigupta1611 <meenakshi.gupta@srijan.net>
2023-02-06 23:26:29 +0530

    VMCF-00: Fixed issues related for paragraph and block entity.

--- js/DisableFieldByViewMode.js
+++ js/DisableFieldByViewMode.js
@@ -1,59 +1,58 @@
 /**
  * @file
- * Defines the Drupal behaviors needed for the Image Widget Crop module.
+ * Defines the Drupal behaviors needed for the Disable field view mode.
  */
 
 (function ($, Drupal) {
   'use strict';
 
   /**
-   * Drupal behavior for the Image Widget Crop module.
+   * Drupal behavior for the Disable field view mode.
    *
    * @type {Drupal~behavior}
    *
    * @prop {Drupal~behaviorAttach} attach
-   *   Attaches the behavior and creates Cropper instances.
+   *   Attaches the behavior and creates instances.
    * @prop {Drupal~behaviorAttach} detach
-   *   Detaches the behavior and destroys Cropper instances.
+   *   Detaches the behavior and destroys instances.
    */
   Drupal.behaviors.disableFieldByViewMode = {
     attach: function (context) {
       this.createInstances(context);
     },
-
     /**
-     * Creates necessary instances of Drupal.ImageWidgetCrop.
+     * Creates necessary instances of Disable field view mode.
      *
      * @param {HTMLElement|jQuery} [context=document]
      *   The context which to find elements in.
      */
     createInstances: function (context) {
-      let viewDisplayMode = $("#edit-view-mode-selection").find(":selected").val();
-      viewDisplayMode = (viewDisplayMode == '_none') ? 'block_content.default' : viewDisplayMode;
-      $(".field-visibility-identifier").once().each(function() {
-        if ($(this).hasClass( viewDisplayMode ) != true) {
-          $(this).hide();
-        }
-        else {
-          $(this).show();
-        }
+
+      let viewDisplayModeId = ($("select[id*='view-mode']").length != 0) ? $("select[id*='view-mode']") : '#edit-view-mode-selection';
+      let viewDisplayMode = extractViewMode($(viewDisplayModeId).find(":selected").val());
+      viewDisplayMode = (viewDisplayMode == '_none') ? 'default' : viewDisplayMode;
+      getViewModeVisibility(viewDisplayMode);
+
+      $(viewDisplayModeId , "select[name^='settings']").on('change', function () {
+        let viewMode = extractViewMode(this.value);
+        ViewMode = (viewMode == '_none') ? 'default' : viewMode;
+        getViewModeVisibility(ViewMode);
+
       });
-
-      $("#edit-view-mode-selection, select[name^='settings']").on('change', function() {
-        let viewMode = this.value;
-        if (viewMode === '_none') {
-          viewMode = 'block_content.default';
-        }
-
-        $(".field-visibility-identifier").each(function() {
-          if ($(this).hasClass( viewMode ) != true) {
+      function getViewModeVisibility(ViewMode) {
+        $(".field-visibility-identifier").once().each(function () {
+          if ($(this).hasClass( viewMode ) != TRUE) {
             $(this).hide();
           }
           else {
             $(this).show();
           }
         });
-      });
+      }
+      function extractViewMode(viewDisplayMode) {
+        return viewDisplayMode.split(".").pop();
+      }
+
     },
   };
 }(jQuery, Drupal));
